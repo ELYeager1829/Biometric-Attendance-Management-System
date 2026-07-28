a4535cc82eee2e28dc5e4acf3bd99b8398562e87
L Thobejane <224060572@tut4life.ac.za>
2026-07-28 13:58:45 +0200

    Update to connectin string

--- frontend/biometric-clocking-frontend/src/services/authServices.js
+++ frontend/biometric-clocking-frontend/src/services/authServices.js
@@ -3,8 +3,8 @@ import axios from "axios";
 console.log(import.meta.env.VITE_API_URL); 
 
 const API = axios.create({
-    baseURL: "http://localhost:5160/api",
-    // baseURL: import.meta.env.VITE_API_URL,
+    //baseURL: "http://localhost:5160/api",
+    baseURL: import.meta.env.VITE_API_URL,
     headers: {
         "Content-Type": "application/json",
     },
